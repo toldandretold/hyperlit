aab5a96c40bded6bbb40d3ba07c1727540bff14f
toldandretold <80452702+toldandretold@users.noreply.github.com>
2025-07-11 07:53:56 +0930

    testing html viewport settings

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hyperlit</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
     
     @yield('styles') <!-- Section for additional CSS files -->
 </head>
