a9fa8bca9d7316da9b4bd635ee66faab400da160
toldandretold <80452702+toldandretold@users.noreply.github.com>
2025-10-12 22:13:51 +1030

    fuck this shit... keyboard cunt (#84)

---
+++ show-ip.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import os from 'os';
+
+function getNetworkIp() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceKey in interfaces) {
+    for (const iface of interfaces[interfaceKey]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return 'localhost';
+}
+
+const ip = getNetworkIp();
+console.log('\n🌐 Network Access Information:');
+console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
+console.log(`\n📱 Access from your phone using:\n`);
+console.log(`   http://${ip}:8000\n`);
+console.log('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n');
+console.log('✅ Make sure your phone is on the same WiFi network\n');
