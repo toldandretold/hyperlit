82bc4ff91c45a961779d71b526a8760d2b32e3ae
toldandretold <80452702+toldandretold@users.noreply.github.com>
2026-01-14 22:00:40 +1030

    scroll restoration defensive add, hopefully maintains position now when code was wiping bfcache on safari ios... i think...
    main fix: book variable for indexedDB was not being refreshed during SPA navigation... which was fucking heaps of shit.

--- resources/js/navigation/pathways/NewBookTransition.js
+++ resources/js/navigation/pathways/NewBookTransition.js
@@ -11,7 +11,7 @@ import { ProgressOverlayEnactor } from '../ProgressOverlayEnactor.js';
 import { glowCloudOrange, glowCloudGreen, glowCloudRed } from '../../components/editIndicator.js';
 import { waitForElementReady, waitForContentReady } from '../../domReadiness.js';
 import { log, verbose } from '../../utilities/logger.js';
-import { debouncedMasterSync, pendingSyncs } from '../../indexedDB/index.js';
+import { debouncedMasterSync, pendingSyncs, updateDatabaseBookId } from '../../indexedDB/index.js';
 import { destroyUserContainer } from '../../components/userContainer.js';
 import { destroyNewBookContainer } from '../../components/newBookButton.js';
 import { destroyHomepageDisplayUnit } from '../../homepageDisplayUnit.js';
@@ -349,6 +349,7 @@ export class NewBookTransition {
     try {
       // Set the current book
       setCurrentBook(bookId);
+      updateDatabaseBookId(bookId);
 
       // Initialize the reader view using the existing system
       await universalPageInitializer(progressCallback);
